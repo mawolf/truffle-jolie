4cb55caff2c39a127f24acf8aff8c1fc83fd3bdb
Martin Wolf <mawol@martinwolf.eu>
2017-03-30 17:00:59 +0200

    Put initial and clean main class

--- src/main/java/jolie/Jolie.java
+++ src/main/java/jolie/Jolie.java
@@ -1,41 +1,7 @@
-/*********************************************************************************
- *   Copyright (C) 2006-2014 by Fabrizio Montesi <famontesi@gmail.com>     *
- *                                                                               *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU Library General Public License as       *
- *   published by the Free Software Foundation; either version 2 of the    *
- *   License, or (at your option) any later version.                         *
- *                                                                               *
- *   This program is distributed in the hope that it will be useful,             *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of              *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               *
- *   GNU General Public License for more details.                                *
- *                                                                               *
- *   You should have received a copy of the GNU Library General Public           *
- *   License along with this program; if not, write to the                       *
- *   Free Software Foundation, Inc.,                                             *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.                   *
- *                                                                               *
- *   For details about the authors of this software, see the AUTHORS file.       *
- *********************************************************************************/
-
 package jolie;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-
-/** Starter class of the Interpreter.
- * @author Fabrizio Montesi
- */
 public class Jolie
 {
-
-    /**
-     * Entry point of program execution.
-     * @param args the command line arguments
-     * TODO Standardize the exit codes.
-     */
     public static void main( String[] args )
     {
     }
